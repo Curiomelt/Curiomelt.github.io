d0c803dc7126f85ebf6b7247f7137799afe3ee28
Curiomelt <136984464+Curiomelt@users.noreply.github.com>
2023-06-30 22:12:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-var url = window.location.origin;
+var url = window.location.href;
 url = url.split('.html')[0];
+//url = url.substring(0, url.lastIndexOf("."));
 window.history.replaceState( null, null, url );
 
 
