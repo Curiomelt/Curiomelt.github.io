de3706d39a12f0646ec197dbb5f6e1e61bcd398e
Curiomelt <136984464+Curiomelt@users.noreply.github.com>
2023-11-06 13:38:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ menuToggleIcon.addEventListener('click', toggleMenu);
 
 
 // Open/Close search form popup
+formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
 
 // -- Close the search form popup on ESC keypress
 window.addEventListener('keyup', (event) => {
