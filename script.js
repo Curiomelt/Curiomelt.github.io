216509f8c427ebb8fb881f712aec8214b77535f1
Curiomelt <136984464+Curiomelt@users.noreply.github.com>
2023-11-07 15:03:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 
-function removeHtmlExtension(url) {
-  return url.replace(/\.html$/, '');
-}
-
-
-window.addEventListener('load', function() {
-  var url = window.location.pathname;
-  url = removeHtmlExtension(url);
-  window.history.replaceState(null, null, url);
-});
-
-
 
 // Grab elements
 const selectElement = (selector) => {
@@ -45,16 +33,13 @@ const toggleMenu = () =>{
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 
-
 // Open/Close search form popup
 formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
 formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
-
 // -- Close the search form popup on ESC keypress
 window.addEventListener('keyup', (event) => {
     if(event.key === 'Escape') searchContainer.classList.remove('activated');
 });
-
 
 // Switch theme/add to local storage
 const body = document.body;
@@ -105,3 +90,6 @@ const swiper = new Swiper(".swiper", {
         }
     }   
 });
+
+
+
